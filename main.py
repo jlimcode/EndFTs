a8810b5757bfc78c4c070af4df8dc3e3fa63282a
Gaurav Kale <gauravkale100@gmail.com>
2022-04-16 08:46:11 -0700

    Refactored main w/ Logging

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import os
+import logging
 import tweepy
 
-KEY = os.getenv("CONSUMER_KEY")
-SECRET = os.getenv("CONSUMER_SECRET")
-TOKEN = os.getenv("ACCESS_TOKEN")
-TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+logger = logging.getLogger()
 
-auth = tweepy.OAuthHandler(KEY, SECRET)
-auth.set_access_token(TOKEN, TOKEN_SECRET)
+def create_api():
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-# Create API object
-api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
 
-print(api)
+def main():
+    api = create_api()
+    
+
+if __name__ == "__main__":
+    main()
