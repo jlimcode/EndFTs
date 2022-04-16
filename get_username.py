ce053be153d245051b0f9fb448b37458a2a1924f
Jason Lim <jmlim@pm.me>
2022-04-16 15:47:50 -0700

    v1 status modifications

--- get_username.py
+++ get_username.py
@@ -1,7 +1,8 @@
 import tweepy
+import tweepy.models
 
-def get_username(api: tweepy.API, reply_tweet: tweepy.Tweet):
-    parent_tweet_id = reply_tweet.conversation_id
+def get_username(api: tweepy.API, reply_tweet: tweepy.models.Status):
+    parent_tweet_id = reply_tweet.in_reply_to_status_id
     parent_tweet = api.get_status(parent_tweet_id)
     parent_user = parent_tweet.user
     return parent_user
