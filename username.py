d18d87f57cfe46f15261edb68f94cb069b59cf12
Alec Goedinghaus <alecgoedinghaus@ucla.edu>
2022-04-16 16:16:54 -0700

    renamed get_username.py

---
+++ username.py
@@ -0,0 +1,16 @@
+import tweepy
+import tweepy.models
+class ParentCommentError(Exception):
+    pass
+
+
+def get_username(api: tweepy.API, reply_tweet: tweepy.models.Status):
+    parent_tweet_id = reply_tweet.in_reply_to_status_id
+
+    # If the tweet that mentions us is a parent tweet
+    if parent_tweet_id is None:
+        raise ParentCommentError
+
+    parent_tweet = api.get_status(parent_tweet_id)
+    parent_user = parent_tweet.user
+    return parent_user
