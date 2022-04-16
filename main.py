20a562d036eefc5a321047d72c35d08f8a6a3a00
Alec Goedinghaus <alecgoedinghaus@ucla.edu>
2022-04-16 13:54:31 -0700

    fixed api/auth

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import os
-import logging
 import tweepy
 
-logger = logging.getLogger()
-
-def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, 
-        wait_on_rate_limit_notify=True)
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        logger.error("Error creating API", exc_info=True)
-        raise e
-    logger.info("API created")
-    return api
+KEY = os.getenv("CONSUMER_KEY")
+SECRET = os.getenv("CONSUMER_SECRET")
+TOKEN = os.getenv("ACCESS_TOKEN")
+TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+ 
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(KEY, SECRET)
+auth.set_access_token(TOKEN, TOKEN_SECRET)
+api = tweepy.API(auth)
+ 
+try:
+   api.verify_credentials()
+   print("Authentication Successful")
+except:
+   print("Authentication Error")
 
 def main():
-    api = create_api()
+    return
     
 
 if __name__ == "__main__":
