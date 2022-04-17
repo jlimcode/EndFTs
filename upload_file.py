72c90f0ebac0521756ee0139d98b50df1147a092
Jason Lim <jmlim@pm.me>
2022-04-16 20:41:51 -0700

    upload json to w3.storage

--- upload_file.py
+++ upload_file.py
@@ -1,10 +1,23 @@
 import os
 import requests
-import json
+import io
+from image_format import image_to_json
 
-def send_post(upload_file):
+def send_post(upload_string):
     url = 'https://api.web3.storage/upload'
-    headers = {'Authorization': os.getenv("WEB3_STORAGE_API")}
-    files = {'file': open(upload_file, 'rb')}
+    headers = {'Authorization': "Bearer " + os.getenv("WEB3_STORAGE_API")}
+    files = io.BytesIO(bytes(upload_string, encoding='utf-8'))
+    files = {'file': io.BufferedReader(raw=files)}
+    
+    r = requests.post(url, headers=headers, files=files)
+    cid = r.json()["cid"]
+    base_url = "https://ipfs.io/ipfs/"
+    return base_url + cid
 
-    requests.post(url, headers=headers, files=files)
+def main():
+    image_url = "https://i.imgur.com/gYMR2Kk.jpeg"
+    url = send_post(image_to_json(image_url))
+    print(url)
+
+if __name__ == "__main__":
+    main()
