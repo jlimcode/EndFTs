d5a985895e530e642acd1b1e8810e3c26c30ac40
Gaurav Kale <gauravkale100@gmail.com>
2022-04-16 21:50:00 -0700

    Pushing P

--- mint.py
+++ mint.py
@@ -1,9 +1,19 @@
 import os
+from turtle import back
 import requests
 import json
-from web3 import Web3
+from web3 import Web3, EthereumTesterProvider
+import unicodedata
 
-w3 = Web3(provider=Web3.HTTPProvider())
+from eth_tester import EthereumTester, PyEVMBackend
+
+e = EthereumTester(backend=PyEVMBackend())
+
+accs = e.get_accounts()
+
+print(accs)
+
+w3 = Web3(provider=EthereumTesterProvider())
 
 contract_address = "0x9486423aA84204a16E46cEf83A0543F3D4150725"
 my_api_key = os.getenv("POLYGONSCAN_API_KEY")
@@ -22,5 +32,7 @@ r_obj = r.json()
 abi = json.loads(r_obj['result'])
 
 contract = w3.eth.contract(address=contract_address, abi=abi)
+b = contract.functions.mint("https://ipfs.io/ipfs/bafkreihxrk33rwxbvmydsbcytoygoc7udozgal4p4m6366hyyyrgaftrim").transact()
+
 
 print(contract)
