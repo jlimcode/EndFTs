e3ff3801a9b21dd301030ccd0e920537060b9cfa
Gaurav Kale <gauravkale100@gmail.com>
2022-04-16 23:21:19 -0700

    Link to NFT in reply_tweet

--- reply_tweet.py
+++ reply_tweet.py
@@ -5,3 +5,10 @@ def base_comment(api: tweepy.API, status_obj: tweepy.models.Status):
     username = status_obj.user.screen_name
     message = f"@{username} read my docs!"
     api.update_status(status=message, in_reply_to_status_id=status_obj.id)
+
+def reply_nft(api: tweepy.API, status_obj: tweepy.models.Status, link_url):
+    status_out = status_obj.in_reply_to_status_id
+    username = status_out.user.screen_name
+    link_out = "https://opensea.io/assets/matic/0x9486423aa84204a16e46cef83a0543f3d4150725/3"
+    message = f"@{username} we seem to own your NFT: /n {link_out}"
+
